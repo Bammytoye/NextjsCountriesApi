6ed7def35364429a56078f1988c60d6cf714eb6b
bammytoye <bamigbalatoyese@gmail.com>
2024-07-11 18:37:57 +0100

    'adjust'

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
 import Countries from "@/components/Countries";
 import Image from "next/image";
-import ApiCountries from "@/components/ApiCountries";
 
 
 export default function Home() {
   return (
     <>
-      {/* <Countries /> */}
-      <ApiCountries />
+      <Countries />
     </>
   );
 }
